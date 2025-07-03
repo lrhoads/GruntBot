91fd2f4a399ea4027c0ecb3ede2a80d10757e2c0
Levi Rhoads <rhoalev@hotmail.com>
2025-07-02 22:04:09 -0500

    Adding base js code

---
+++ src/commands.js
@@ -0,0 +1,9 @@
+export const GRUNT_COMMAND = {
+	name: 'grunt',
+	description: 'Poke the grunt.',
+  };
+  
+  export const INVITE_COMMAND = {
+	name: 'invite',
+	description: 'Get an invite link to add the bot to your server',
+  };
